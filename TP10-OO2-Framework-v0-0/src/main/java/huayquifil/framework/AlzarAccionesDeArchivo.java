142023058aa2ab60f9288b7d20a3618427099427
rhuayquifil <ezehuayquifil@hotmail.com>
2023-06-20 02:18:52 -0300

    implementacion de alzar acciones de archivo .json

--- TP10-OO2-Framework-v0-0/src/main/java/huayquifil/framework/AlzarAccionesDeArchivo.java
+++ TP10-OO2-Framework-v0-0/src/main/java/huayquifil/framework/AlzarAccionesDeArchivo.java
@@ -1,35 +1,81 @@
 package huayquifil.framework;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.Properties;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
 public class AlzarAccionesDeArchivo implements AlzarAcciones {
 
-	private String path;
+	private String filePath;
 
-	public AlzarAccionesDeArchivo(String path) {
-		Objects.requireNonNull(path);
+	public AlzarAccionesDeArchivo(String filePath) {
+		Objects.requireNonNull(filePath);
 
-		if (path.isEmpty() || path.isBlank()) {
+		if (filePath.isEmpty() || filePath.isBlank()) {
 			throw new RuntimeException("path no encontrado");
 		}
 
-		this.path = path;
+		this.filePath = filePath;
 	}
 
 	@Override
 	public HashMap<Integer, Accion> alzar() {
-		Properties properties = new Properties();
-
 		HashMap<Integer, Accion> lista = new HashMap<>();
+
+		if (isJSONFile()) {
+			return alzarDeArchivoJSON(lista);
+		}
+		return alzarDeArchivoProperties(lista);
+	}
+
+	private HashMap<Integer, Accion> alzarDeArchivoJSON(HashMap<Integer, Accion> lista) {
+		Gson gson = new GsonBuilder().create();
+		int i = 0;
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(this.filePath))) {
+			JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+			JsonArray jsonArray = jsonObject.getAsJsonArray("acciones");
+			for (JsonElement jsonElement : jsonArray) {
+				String clase = jsonElement.getAsString();
+				Accion nuevaAccion = (Accion) Class.forName(clase).getDeclaredConstructor().newInstance();
+				lista.put(i, nuevaAccion);
+				i++;
+			}
+		} catch (IOException | JsonSyntaxException | JsonIOException | InstantiationException | IllegalAccessException
+				| ClassNotFoundException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
+				| SecurityException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	private boolean isJSONFile() {
+		Path path = Path.of(this.filePath);
+		String fileName = path.getFileName().toString();
+		return fileName.endsWith(".json");
+	}
+
+	private HashMap<Integer, Accion> alzarDeArchivoProperties(HashMap<Integer, Accion> lista) {
+		Properties properties = new Properties();
 
 		// lee el archivo de properties y va generando la lista con todas las
 		// implementaciones propias del usuario del framework
-		try (InputStream config = getClass().getResourceAsStream(this.path);) {
+		try (InputStream config = getClass().getResourceAsStream(this.filePath);) {
 			properties.load(config);
 
 			String archivo = properties.getProperty("acciones");
@@ -51,5 +97,4 @@ public class AlzarAccionesDeArchivo implements AlzarAcciones {
 
 		return lista;
 	}
-
 }
--- TP10-OO2-Framework-v0-0/src/main/java/huayquifil/main/Main.java
+++ TP10-OO2-Framework-v0-0/src/main/java/huayquifil/main/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Framework framework = new Framework(new AlzarAccionesDeArchivo("/acciones.properties"));
+//		Framework framework = new Framework(new AlzarAccionesDeArchivo("/acciones.properties"));
+
+		Framework framework = new Framework(new AlzarAccionesDeArchivo(
+				"C:\\Users\\ezehu\\git\\TP10-OO2-Framework-v0-0\\TP10-OO2-Framework-v0-0\\src\\main\\resources\\acciones.json"));
+
 		framework.init();
 	}
 
